97d11cd2edb898ba15d21f3f3970f21d7bda584b
Suganth-coder <sugupr1234@gmail.com>
2022-02-28 04:47:34 +0530

    temp dev commit

--- js/include.js
+++ js/include.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     $("#side-nav").load('../assets/side-nav.html');
-    $(".header").load("../assets/header.html");
+    $(".header").load("../../assets/header.html");
 });
