d373839d7f314c80ceb517fde656257eda3f6c5b
Suganth-coder <sugupr1234@gmail.com>
2022-02-27 21:28:26 +0530

    partial completion of dashboard page

---
+++ js/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    // $.get("https://www.metals-api.com/api/carat?access_key=ke45yxbyjqh3k6h2js5mqq3evxk8rlo22ifaa0n0p4rgymnwlkhg97ps3tn2", function(data, status) {
+
+    //     let rate_limit = 4,
+    //         current = 0;
+    //     $.each(data.rates, function(key, value) {
+    //         if (current <= 3) {
+    //             console.log(".k1");
+    //         }
+    //     });
+    // });
+
+});
